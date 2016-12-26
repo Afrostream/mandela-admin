5b186e92cdffd8bb8da8b1ef0f23812d16a87461
benjipott <pott.benjamin@gmail.com>
2016-12-26 16:14:44 +0100

    add simple background

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render () {
     return (
       <div className="App">
+        <div className="app-background"/>
         <div className="container">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo"/>
