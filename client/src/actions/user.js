053b18aee0a5988907698e4baba7c3c2c665c71f
Francois Dabiel BroCode <brocode@ProCode.local>
2016-12-28 14:43:12 +0100

    user signIn ok

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,5 +1,10 @@
 import ActionTypes from '../consts/ActionTypes'
 import { browserHistory } from 'react-router'
+import User from '../lib/api/User'
+
+export function getUser() {
+  return User.get()
+}
 
 export function setUser(user) {
   console.log('SetUser :', user)
@@ -11,9 +16,12 @@ export function setUser(user) {
 
 export function logUser({ name, password }) {
   return (dispatch, getState) => {
-    setTimeout(() => {
-      dispatch(setUser({ name, password }))
-      browserHistory.push('/')
-    }, 1000)
+    User.signIn({
+      email: name,
+      password
+    }).then(getUser)
+      .then((user) => dispatch(setUser(user)))
+      .then(() =>  browserHistory.push('/'))
+      .catch(err => console.warn('Log error :', err))
   }
 }
