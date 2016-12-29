8d2b78757859036431c8d4b7eb85b596be14d41e
Francois Dabiel BroCode <brocode@ProCode.local>
2016-12-29 09:39:25 +0100

    added CORS

--- server/app/app.js
+++ server/app/app.js
@@ -33,6 +33,12 @@ switch (process.env.NODE_ENV) {
     break;
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var routes = require('./routes.js');
 app.use(routes);
 app.get('*', (req, res) => {
